cb15e3c52d1cfdeb81c2f09574b3ac5ffa6b6084
hornermar <hornerova.m@gmail.com>
2025-11-03 11:21:22 +0100

    hide sykora

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import "./Detail.css";
 import { find } from "lodash";
 import { DetailNotFound } from "./DetailNotFound";
+import { Note } from "../../types/ProjectType";
 
 export const Detail = () => {
   const { project } = useParams<{ project: string }>();
@@ -12,7 +13,7 @@ export const Detail = () => {
 
   if (!detailData) return <DetailNotFound />;
 
-  const { code, title, description, why, technology, image, url } = detailData;
+  const { code, title, description, why, technology, image, url, note } = detailData;
 
   const linkOnline = url ? url : `https://marketahajek.cz/${code}`;
 
@@ -24,7 +25,7 @@ export const Detail = () => {
 
       <div className="detail__text">
         <h2>{title}</h2>
-        {
+        {note !== Note.inProgress &&
           <Link to={linkOnline} reloadDocument className="online">
             ➜ Online tady
           </Link>
